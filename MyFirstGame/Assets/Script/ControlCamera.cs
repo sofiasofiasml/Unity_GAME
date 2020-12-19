dfd8cdd3aa08522f3fdecd68726b2dae967a2a41
sofiasofiasml <romata1966@gmail.com>
2020-12-19 20:38:11 +0100

    movimiento vertical y horizontal

---
+++ MyFirstGame/Assets/Script/ControlCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ControlCamera : MonoBehaviour
+{
+    private Vector3 desplace = new Vector3(4, 11, -14);
+    public GameObject Van; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Van.transform.position + desplace; 
+    }
+}
--- MyFirstGame/Assets/Script/ControlGamer.cs
+++ MyFirstGame/Assets/Script/ControlGamer.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ControlGamer : MonoBehaviour
 {
+    public float velocity = 20.0f; //if this variable is public, you can change in unity program 
+    public float velocityTurn = 5;
+    public float controlHorizontal;
+    public float controlVertical; 
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +17,12 @@ public class ControlGamer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //read de input the diferents axis
+        controlHorizontal = Input.GetAxis("Horizontal");
+        controlVertical = Input.GetAxis("Vertical"); 
+
         //changing the position of the van on the z-axis
-        transform.Translate(Vector3.forward * Time.deltaTime * 15); 
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * velocity * controlVertical);
+        transform.Translate(Vector3.right * Time.deltaTime * velocityTurn * controlHorizontal); 
     }
 }
