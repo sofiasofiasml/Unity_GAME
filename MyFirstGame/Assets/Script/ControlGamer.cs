44199bd2a0a809a9e2fc16042ac13d771ca93f26
sofiasofiasml <romata1966@gmail.com>
2020-12-19 12:44:38 +0100

    first_practice
    van move in the z-axys

---
+++ MyFirstGame/Assets/Script/ControlGamer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlGamer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //changing the position of the van on the z-axis
+        transform.Translate(Vector3.forward * Time.deltaTime * 15); 
+        
+    }
+}
