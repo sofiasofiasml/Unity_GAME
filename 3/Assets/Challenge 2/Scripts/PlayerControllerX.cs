673556410d4e5a78f2f78d464df734d41b66e9a7
sofiasofiasml <romata1966@gmail.com>
2020-12-23 20:20:41 +0100

    Dogs

---
+++ 3/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float TimepoEspera = 1;
+    private float TimepoSiguienteDisparo = 0; 
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > TimepoSiguienteDisparo)
+        {
+            TimepoSiguienteDisparo = Time.time + TimepoEspera; 
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
